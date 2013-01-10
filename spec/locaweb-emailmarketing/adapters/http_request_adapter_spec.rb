4c485b29c7dc729594015f9ba560f93938f21f15
Fabio Perrella <fabio.perrella@gmail.com>
2013-01-10 13:53:27 -0200

    pending tests for http adapter

--- spec/locaweb-emailmarketing/adapters/http_request_adapter_spec.rb
+++ spec/locaweb-emailmarketing/adapters/http_request_adapter_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 include Locaweb::Emailmarketing
 
 describe Locaweb::Emailmarketing::HttpRequestAdapter do
+  let(:subject) { HttpRequestAdapter.new(auth_token: AUTH_TOKEN, base_url: BASE_URL) }
+
   describe ".initialize" do
     context "when valid options" do
       it "does not raise exception" do
@@ -18,14 +20,22 @@ describe Locaweb::Emailmarketing::HttpRequestAdapter do
   end
 
   describe ".get" do
-    it "does something" do
-      pending
+    it "calls restclient GET with correct options" do
+      RestClient.should_receive(:get).with("#{BASE_URL}/aaa", "X-Auth-Token" => AUTH_TOKEN, accept: :json).and_return "{}"
+      subject.get "aaa"
+    end
+
+    it "returns JSON of http response body" do
+      RestClient.stub(:get).and_return %q|{"aaaa":"123"}|
+      subject.get("aaa").should == {"aaaa" => "123"}
     end
   end
 
   describe ".put" do
-    it "does something" do
-      pending
+    it "calls restclient PUT with correct options" do
+      attributes = {aa: 1}
+      RestClient.should_receive(:put).with("#{BASE_URL}/aaa", attributes.to_json, "X-Auth-Token" => AUTH_TOKEN, content_type: :json).and_return "{}"
+      subject.put "aaa", attributes
     end
   end
 end
