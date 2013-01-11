504119eab0e950f3b231fd878818eceb0834f717
Fabio Perrella <fabio.perrella@gmail.com>
2013-01-11 17:09:28 -0200

    custom_field client using base_client

--- lib/locaweb-emailmarketing/clients/custom_field_client.rb
+++ lib/locaweb-emailmarketing/clients/custom_field_client.rb
@@ -1,32 +1,11 @@
 module Locaweb
   module Emailmarketing
-    class CustomFieldClient
+    class CustomFieldClient < BaseClient
       def initialize(http_request_adapter, account_id)
-        @http_request_adapter = http_request_adapter
-        @account_id = account_id
-      end
-
-      def all
-        @http_request_adapter.get "accounts/#{@account_id}/custom_fields"
-      end
-
-      def get(id)
-        @http_request_adapter.get "accounts/#{@account_id}/custom_fields/#{id}"
-      end
-
-      def create(attributes)
-        attributes.assert_required_keys(required: [:name, :type])
-        response_json = JSON @http_request_adapter.post "accounts/#{@account_id}/custom_fields", {custom_field: attributes}
-        response_json["id"]
-      end
-
-      def update(id, attributes)
-        attributes.assert_required_keys(required: [:name])
-        @http_request_adapter.put "accounts/#{@account_id}/custom_fields/#{id}", {custom_field: attributes}
-      end
-
-      def destroy(id)
-        @http_request_adapter.delete "accounts/#{@account_id}/custom_fields/#{id}"
+        super
+        @resource_name = "custom_fields"
+        @required_keys_to_update = { required: [:name] }
+        @required_keys_to_create = { required: [:name, :type] }
       end
     end
   end
