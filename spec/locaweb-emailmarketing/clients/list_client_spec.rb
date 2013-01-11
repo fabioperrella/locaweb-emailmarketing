c913f5b61ed5c7972124059860d80a88cc575ae8
Fabio Perrella <fabio.perrella@gmail.com>
2013-01-11 19:56:34 -0200

    list.get

--- spec/locaweb-emailmarketing/clients/list_client_spec.rb
+++ spec/locaweb-emailmarketing/clients/list_client_spec.rb
@@ -13,4 +13,60 @@ describe Locaweb::Emailmarketing::ListClient do
       end
     end
   end
+
+  describe ".get" do
+    it "returns a list" do
+      VCR.use_cassette('list_get') do
+        client.lists.create(name: "list 2", description: "lalapopo")
+        list_id = client.lists.all["items"].find{|c| c["name"] == "list 2"}["id"]
+        client.lists.get(list_id).should include("name" => "list 2", "description" => "lalapopo")
+      end
+    end
+  end
+
+  describe ".create" do
+    it "creates a list and return list_id" do
+      pending
+      VCR.use_cassette('list_create') do
+        if list = client.lists.all["items"].find{|c| c["name"] == "codigo"}
+          client.lists.destroy list["id"]
+        end
+        list_id = client.lists.create(name: "codigo", type: "number")
+        client.lists.get(list_id).should include({"id"=>list_id, "name"=>"codigo", "type"=>"number"})
+      end
+    end
+  end
+
+  describe ".update" do
+    it "updates a list" do
+      pending
+      VCR.use_cassette('list_update') do
+        lists = client.lists.all["items"]
+        if list = lists.find{|c| c["name"] == "city"}
+          client.lists.destroy list["id"]
+        end
+
+        list = client.lists.all["items"].find{|c| c["name"] == "Cidade"}
+        list_id = if list
+          list["id"]
+        else
+          client.lists.create(name: "Cidade", type: "string")
+        end
+
+        client.lists.update(list_id, name: "city")
+        client.lists.get(list_id)["name"].should == "city"
+      end
+    end
+  end
+
+  describe ".destroy" do
+    it "destroys a list" do
+      pending
+      VCR.use_cassette('list_destroy') do
+        list_id = client.lists.create(name: "lalapopo", type: "number")
+        client.lists.destroy(list_id)
+        client.lists.get(list_id).should be_nil
+      end
+    end
+  end
 end
