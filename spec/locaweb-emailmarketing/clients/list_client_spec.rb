2f4be478ab6f99251601a4b1369415b67447e79d
Fabio Perrella <fabio.perrella@gmail.com>
2013-01-14 16:33:04 -0200

    testing base_client create method

--- spec/locaweb-emailmarketing/clients/list_client_spec.rb
+++ spec/locaweb-emailmarketing/clients/list_client_spec.rb
@@ -3,5 +3,7 @@ require "spec_helper"
 include Locaweb::Emailmarketing
 
 describe Locaweb::Emailmarketing::ListClient do
-  it_should_behave_like "a base client", "lists"
+  it_should_behave_like "a base client", "lists" do
+    let(:requires_keys_to_create) { [:name] }
+  end
 end
--- spec/support/base_client_examples.rb
+++ spec/support/base_client_examples.rb
@@ -2,7 +2,7 @@ shared_examples_for "a base client" do |resource_name|
   let(:client) { Client.new auth_token: AUTH_TOKEN, base_url: BASE_URL, account_id: TRIAL_ACCOUNT_ID }
 
   describe ".all" do
-    it "calls http GET to resource index url" do
+    it "calls http GET to #{resource_name} index url" do
       HttpRequestAdapter.any_instance.should_receive(:get).with("accounts/#{TRIAL_ACCOUNT_ID}/#{resource_name}")
       client.send(resource_name).all
     end
@@ -16,7 +16,7 @@ shared_examples_for "a base client" do |resource_name|
   describe ".get" do
     let(:resource_id) { "lalapopolala" }
 
-    it "calls http GET to resource show url" do
+    it "calls http GET to #{resource_name} show url" do
       HttpRequestAdapter.any_instance.should_receive(:get).with("accounts/#{TRIAL_ACCOUNT_ID}/#{resource_name}/#{resource_id}")
       client.send(resource_name).get(resource_id)
     end
@@ -24,6 +24,21 @@ shared_examples_for "a base client" do |resource_name|
     it "returns GET response" do
       HttpRequestAdapter.any_instance.stub(:get).and_return({})
       client.send(resource_name).get(resource_id).should == {}
+    end
+  end
+
+  describe ".create" do
+    context "when invalid attributes" do
+      it "raise ArgumentError exception" do
+        lambda{ client.send(resource_name).create(lalapopo: 1) }.should raise_exception ArgumentError
+      end
+    end
+
+    it "calls http POST to #{resource_name} create url" do
+      attributes = { }
+      requires_keys_to_create.each { |key| attributes[key] = "any_value" }
+      HttpRequestAdapter.any_instance.should_receive(:post).with("accounts/#{TRIAL_ACCOUNT_ID}/#{resource_name}", {resource_name.singularize.to_sym => attributes}).and_return({"id" => 12})
+      client.send(resource_name).create(attributes)
     end
   end
 end
