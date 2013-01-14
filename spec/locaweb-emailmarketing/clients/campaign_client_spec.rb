515ae01c463feb9883f9d7b8eeb7ea0d451015ad
Fabio Perrella <fabio.perrella@gmail.com>
2013-01-14 17:00:44 -0200

    using shared examples to test

--- spec/locaweb-emailmarketing/clients/campaign_client_spec.rb
+++ spec/locaweb-emailmarketing/clients/campaign_client_spec.rb
@@ -3,55 +3,8 @@ require "spec_helper"
 include Locaweb::Emailmarketing
 
 describe Locaweb::Emailmarketing::CampaignClient do
-  let(:client) { Client.new auth_token: AUTH_TOKEN, base_url: BASE_URL, account_id: TRIAL_ACCOUNT_ID }
-
-  describe ".all" do
-    it "returns all campaigns" do
-      VCR.use_cassette('campaign_all') do
-        client.campaigns.create(name: "camp 2")
-        client.campaigns.all["items"].find{|c| c["name"] == "camp 2"}.should include("name"=>"camp 2")
-      end
-    end
-  end
-
-  describe ".get" do
-    it "returns a campaign" do
-      VCR.use_cassette('campaign_get') do
-        campaign_id = client.campaigns.create(name: "camp 3")
-        client.campaigns.get(campaign_id).should include("name"=>"camp 3")
-      end
-    end
-  end
-
-  describe ".create" do
-    it "creates a campaign and return campaign_id" do
-      VCR.use_cassette('campaign_create') do
-        if campaigns = client.campaigns.all["items"].find{|c| c["name"] == "campanha 1"}
-          client.campaigns.destroy campaigns["id"]
-        end
-        campaign_id = client.campaigns.create(name: "campanha 1", description: "desc camp 1")
-        client.campaigns.get(campaign_id).should include({"id"=>campaign_id, "name"=>"campanha 1"})
-      end
-    end
-  end
-
-  describe ".update" do
-    it "updates a campaign" do
-      VCR.use_cassette('campaign_update') do
-        campaign_id = client.campaigns.create(name: "camp 7", description: "desc camp 1")
-        client.campaigns.update(campaign_id, name: "camp 77")
-        client.campaigns.get(campaign_id)["name"].should == "camp 77"
-      end
-    end
-  end
-
-  describe ".destroy" do
-    it "destroys a campaign" do
-      VCR.use_cassette('campaign_destroy') do
-        campaign_id = client.campaigns.create(name: "camp 6", description: "desc camp 1")
-        client.campaigns.destroy(campaign_id)
-        client.campaigns.get(campaign_id).should be_nil
-      end
-    end
+  it_should_behave_like "a base client", "campaigns" do
+    let(:requires_keys_to_create) { [:name] }
+    let(:requires_keys_to_update) { [:name] }
   end
 end
--- spec/locaweb-emailmarketing/clients/custom_field_client_spec.rb
+++ spec/locaweb-emailmarketing/clients/custom_field_client_spec.rb
@@ -3,66 +3,8 @@ require "spec_helper"
 include Locaweb::Emailmarketing
 
 describe Locaweb::Emailmarketing::CustomFieldClient do
-  let(:client) { Client.new auth_token: AUTH_TOKEN, base_url: BASE_URL, account_id: TRIAL_ACCOUNT_ID }
-
-  describe ".all" do
-    it "returns all custom_fields" do
-      VCR.use_cassette('custom_field_all') do
-        client.custom_fields.create(name: "Cidade", type: "string")
-        client.custom_fields.all["items"].find{|c| c["name"] == "Cidade"}.should include("name"=>"Cidade", "type"=>"string")
-      end
-    end
-  end
-
-  describe ".get" do
-    it "returns a custom_field" do
-      VCR.use_cassette('custom_field_get') do
-        custom_field_id = client.custom_fields.all["items"].find{|c| c["name"] == "Cidade"}["id"]
-        client.custom_fields.get(custom_field_id).should include("name"=>"Cidade", "type"=>"string")
-      end
-    end
-  end
-
-  describe ".create" do
-    it "creates a custom_field and return custom_field_id" do
-      VCR.use_cassette('custom_field_create') do
-        if custom_field = client.custom_fields.all["items"].find{|c| c["name"] == "codigo"}
-          client.custom_fields.destroy custom_field["id"]
-        end
-        custom_field_id = client.custom_fields.create(name: "codigo", type: "number")
-        client.custom_fields.get(custom_field_id).should include({"id"=>custom_field_id, "name"=>"codigo", "type"=>"number"})
-      end
-    end
-  end
-
-  describe ".update" do
-    it "updates a custom_field" do
-      VCR.use_cassette('custom_field_update') do
-        custom_fields = client.custom_fields.all["items"]
-        if custom_field = custom_fields.find{|c| c["name"] == "city"}
-          client.custom_fields.destroy custom_field["id"]
-        end
-
-        custom_field = client.custom_fields.all["items"].find{|c| c["name"] == "Cidade"}
-        custom_field_id = if custom_field
-          custom_field["id"]
-        else
-          client.custom_fields.create(name: "Cidade", type: "string")
-        end
-
-        client.custom_fields.update(custom_field_id, name: "city")
-        client.custom_fields.get(custom_field_id)["name"].should == "city"
-      end
-    end
-  end
-
-  describe ".destroy" do
-    it "destroys a custom_field" do
-      VCR.use_cassette('custom_field_destroy') do
-        custom_field_id = client.custom_fields.create(name: "lalapopo", type: "number")
-        client.custom_fields.destroy(custom_field_id)
-        client.custom_fields.get(custom_field_id).should be_nil
-      end
-    end
+  it_should_behave_like "a base client", "custom_fields" do
+    let(:requires_keys_to_create) { [:name, :type] }
+    let(:requires_keys_to_update) { [:name] }
   end
 end
